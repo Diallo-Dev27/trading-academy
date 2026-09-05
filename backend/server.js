6698961b41a323ef546fabbe25161764cceb56ad
Diallo Yassne <Diallomamadouyassne@gmail.com>
2026-09-05 17:48:00 +0530

    remove dotenv for render

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const SibApiV3Sdk = require('@getbrevo/brevo');
-require('dotenv').config();
+
 
 const app = express();
 app.use(cors());
